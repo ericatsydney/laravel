ac37bbaa4bcc077d39b07441633d70bc99c03488
Eric Tan <eric.g.x.tan@gmail.com>
2015-10-25 22:39:59 +1000

    Fixed/ updated the gulp and add the leafletjs in.

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password','address','lat','lng'];
 
     /**
      * The attributes excluded from the model's JSON form.
