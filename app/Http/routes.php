2d90240379a8719b52c510b98f12a3371a87ad17
ericatsydney <eric.g.x.tan@gmail.com>
2015-11-08 22:39:06 +1000

    Update the seeder php.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,9 +23,18 @@ Route::resource('blog','BlogController');
 //post('blog','BlogController@store');
 //get('blog/{slug}/edit','BlogController@edit');
 
-Route::controllers([
-   'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController',
-]);
+//Route::controllers([
+//   'auth' => 'Auth\AuthController',
+//    'password' => 'Auth\PasswordController',
+//]);
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::resource('user','UserController');
