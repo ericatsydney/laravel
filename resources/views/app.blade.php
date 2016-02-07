62e6370a50119c7a95278a6c7044db7ec97f3a39
alskk008 <mack.xu@groupgo.com.au>
2016-02-07 23:43:41 +1000

    Update the template with Bootswatch's Ubuntu's Orange.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,16 @@
   <link rel='stylesheet' href='{{ URL::asset('/css/MarkerCluster.css') }}' />
   <link rel='stylesheet' href='{{ URL::asset('/css/Control.Geocoder.css') }}' />
   <link rel='stylesheet' href='{{ URL::asset('/css/MarkerCluster.Default.css') }}' />
-  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'/>
-  <link href='{{ URL::asset('/css/animate.min.css') }}' rel='stylesheet'/>
-  <link href='{{ URL::asset('/css/lightbox.css') }}' rel='stylesheet'/>
-  <link href='{{ URL::asset('/css/main.css') }}' rel='stylesheet'/>
-  <link href='{{ URL::asset('/css/responsive.css') }}' rel='stylesheet'/>
+  {{--<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'/>--}}
+  {{--<link href='{{ URL::asset('/css/animate.min.css') }}' rel='stylesheet'/>--}}
+  {{--<link href='{{ URL::asset('/css/lightbox.css') }}' rel='stylesheet'/>--}}
+  {{--<link href='{{ URL::asset('/css/main.css') }}' rel='stylesheet'/>--}}
+  {{--<link href='{{ URL::asset('/css/responsive.css') }}' rel='stylesheet'/>--}}
+    <link href='{{ URL::asset('/css/bootstrap.min.css') }}' rel='stylesheet'/>
 </head>
 <body>
+  @include('layout.navigation')
+  @yield('jumbotron')
   <div class='container'>
       @if (Session::has('flash_message'))
           <div class='alert alert-success'>
@@ -24,10 +27,10 @@
       @endif
 
       @yield('content')
+      @include('layout.footer')
   </div> <!-- end of container -->
   <script src='//code.jquery.com/jquery.js'></script>
   <script src='http://cdn.leafletjs.com/leaflet-0.7.5/leaflet.js'></script>
   <script src='{{ URL::asset('/js/min/all.js') }}'></script>
-  @yield('footer')
 </body>
 </html>
---
+++ resources/views/component/jumbotron.blade.php
@@ -0,0 +1,19 @@
+<div class="jumbotron">
+    <div class="container">
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="form-group">
+                    <label for="inputEmail" class="col-lg-2 control-label">Find local printer</label>
+                    <div class="col-lg-10">
+                        <input type="text" class="form-control" id="inputEmail" placeholder="Enter your suburb or postcode(e.g. 4000)">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-lg-10 col-lg-offset-2">
+                        <button type="submit" class="btn btn-primary">Send</button>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+</div>
